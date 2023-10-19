fa13e86cec211c0bf1af377a07b053112623f632
Mohammad Babaei <44874325+IranDec@users.noreply.github.com>
2023-10-20 02:34:48 +0300

    Update lovegamematch.py

--- love check/lovegamematch.py
+++ love check/lovegamematch.py
@@ -18,9 +18,9 @@ second_digit = l + o + v + e
   
 score = int(str(first_digit) + str(second_digit))
 
-if score < 10 and score >90:
-  print(f"Your score is *{score}*, you go together like coke and mentos.")
-elif score > 40 and  score <50:
-  print(f"Your score is *{score}*, you are alright together.")
+if (score < 10) or (score > 90):
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+  print(f"Your score is {score}, you are alright together.")
 else:
-  print(f"Your score is *{score}*.")
+  print(f"Your score is {score}.")
